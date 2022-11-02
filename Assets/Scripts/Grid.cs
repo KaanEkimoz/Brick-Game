e80643e562b3c7a597deba0d30b306f52dd3f63e
Yusuf Kaan Usta <205040053@stu.istinye.edu.tr>
2022-11-02 15:46:09 +0300

    Starter Files Added.
    -added starter files

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Grid : MonoBehaviour
+{
+    public static Grid GameGrid { get; set; }
+    public GameObject borderObject;
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+    private Vector2 spawnPosition;
+
+    public GameObject[] tetrominos;
+    
+    public int GridWidth
+    {
+        get => gridWidth;
+        set => gridWidth = value;
+    }
+    public  int GridHeight
+    {
+        get => gridHeight;
+        set => gridHeight = value;
+    }
+    void Start()
+    {
+        transform.position = Round(transform.position);
+        
+        
+        CreateGridBorders();
+        spawnPosition = CalculateSpawnPosition();
+        SpawnRandomTetromino();
+        GameGrid = GetComponent<Grid>();
+    }
+
+    private void CreateGridBorders()
+    {
+        for (float i = 0; i < gridWidth; i++)
+        {
+            Vector3 cellPosition = new Vector3(i, 0, 0);
+            Instantiate(borderObject,Round(transform.position + cellPosition),Quaternion.identity,transform.parent);
+        }
+
+        for (float i = 0; i < gridHeight; i++)
+        {
+            Vector3 cellPosition = new Vector3(0, i, 0);
+            Instantiate(borderObject,Round(transform.position + cellPosition),Quaternion.identity,transform.parent);
+            cellPosition = new Vector3(gridWidth, i, 0);
+            Instantiate(borderObject,Round(transform.position + cellPosition),Quaternion.identity,transform.parent);
+        }
+    }
+    
+    
+    public bool CheckIsInsideGrid(Vector2 pos)
+    {
+        bool checkXBorder = Round(pos).x < transform.position.x + GridWidth && pos.x > transform.position.x;
+        bool checkYBorder = Round(pos).y > transform.position.y;
+        return checkXBorder && checkYBorder;
+    }
+    
+
+    public Vector2 Round(Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+    public Vector3 Round(Vector3 pos)
+    {
+        return new Vector3(Mathf.Round(pos.x), Mathf.Round(pos.y), Mathf.Round((pos.z)));
+    }
+
+    public void SpawnRandomTetromino()
+    {
+        Instantiate(tetrominos[RandomTetrominoNumber()], spawnPosition, transform.rotation);
+    }
+
+    private Vector3 CalculateSpawnPosition()
+    {
+        int spawnYOffset = gridHeight + 1;
+        int spawnXOffset = gridWidth / 2;
+        return new Vector3(transform.position.x + spawnXOffset, transform.position.y + spawnYOffset, 0f);
+    }
+
+    public int RandomTetrominoNumber()
+    {
+        int randomTetromino = Random.Range(0, 7);
+        return randomTetromino;
+    }
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    float fallTimer;
+    //fall speed in minutes
+    public float fallSpeedInSeconds = 1;
+    public bool canAutoFall = true;
+    public bool allowRotation = true;
+    void Update()
+    {
+        CheckUserInput();
+        MoveDownWithTime();
+
+    }
+    void CheckUserInput()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+            MoveDown();
+        //if (Input.GetKeyDown(KeyCode.A))
+            //MoveRight();
+    }
+    private void Rotate()
+    {
+        transform.Rotate(0, 0, -90);
+        foreach (Transform mino in transform)
+        {
+            if (Grid.GameGrid.CheckIsInsideGrid(mino.position) == false)
+            {
+                if (mino.position.y < Grid.GameGrid.transform.position.y)
+                    mino.position += Vector3.up;
+            }
+        }
+    }
+    void MoveDownWithTime()
+    {
+        if (IsTimePeriodPassed() && CanMoveDown())
+        {
+            transform.position += new Vector3(0, -1, 0);
+            fallTimer = Time.time;
+        }
+    }
+
+    void MoveDown()
+    {
+        if(CanMoveDown())
+            transform.position += new Vector3(0, -1, 0);
+    }
+    bool CanMoveDown()
+    {
+        foreach(Transform mino in transform)
+        {
+            Vector3 nextPos = Grid.GameGrid.Round(mino.position + Vector3.down);
+                
+            if(Grid.GameGrid.CheckIsInsideGrid(nextPos) == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool IsTimePeriodPassed()
+    {
+        return Time.time - fallTimer >= fallSpeedInSeconds && canAutoFall;
+    }
+}
